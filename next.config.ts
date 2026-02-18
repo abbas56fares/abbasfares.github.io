16f46bf90939cc9c1e59716dce07ac77e11bda2f
abbas ali fares <abbasalifares@Mac.netis.cc>
2026-02-18 16:49:26 +0200

    123

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // Uncomment the basePath below if your repo is NOT named 'your-username.github.io'
-  // basePath: '/your-repo-name',
 };
 
 export default nextConfig;
